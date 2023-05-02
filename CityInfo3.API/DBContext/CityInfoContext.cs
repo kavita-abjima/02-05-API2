65acd9ede950c02746a3994b5d13744b3f9d6cbc
Kavita <kavita.jangir@abjima.com>
2023-05-02 18:50:00 +0530

    API

---
+++ CityInfo3.API/DBContext/CityInfoContext.cs
@@ -0,0 +1,18 @@
+﻿
+using CityInfo3.API.Entities;
+using System.Collections.Generic;
+
+namespace CityInfo3.API.DBContext
+{
+    public class CityInfoContext:DBContext
+    {
+        public DbSet<City> Cities { get; set; } = null!;
+        public DbSet<PointOfInterest> PointsOfInterest { get; set; } = null!;
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlite("connectionstring");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
